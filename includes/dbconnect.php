e5555ed2817ca28bcaa72c84c3581176822a7450
Todd Rosner <todd@vivisurf.com>
2013-07-02 20:57:11 -0700

    Updated dbconnect.php

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -3,7 +3,7 @@
 // CONNECT TO THE DATABASE
 $DB_NAME = 'opsworks_book_demo';
 $DB_HOST = 'ec2-54-227-53-6.compute-1.amazonaws.com'; // CHANGE TO YOUR OPSWORKS MYSQL SERVER IP ADDRESS
-$DB_USER = 'root'; // CHANGE TO root
+$DB_USER = 'root'; // KEEP IN PLACE AS PER CHAPTER 4 
 $DB_PASS = '8ePO2O8E0UHG91975I3k'; // KEEP IN PLACE AS PER CHAPTER 4
 					
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
